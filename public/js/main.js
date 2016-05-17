0422c6ec2ddca130291bda293ebac2b0fdf3b6f0
Timmy823 <boss63081@gmail.com>
2016-05-17 16:48:50 +0800

    custom_feedback page is over

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('div#nav-bar')
+    .on('click', 'div.myitem', function(e){
+      if(!$(this).hasClass('active')) {
+        $(this)
+          .addClass('active')
+          .siblings('div.myitem')
+          .removeClass('active');
+      }     
+    });
+});
