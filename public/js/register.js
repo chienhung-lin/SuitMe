095fabfdca7739030dc5a3c12ddce8ad0529bd75
Chen Hung, Lin <kk357258@gmail.com>
2016-05-30 21:51:46 +0800

    extract js and css file from hbs

---
+++ public/js/register.js
@@ -0,0 +1,118 @@
+$(document).ready( function() {
+
+  $("form#register-form").validate({
+    debug: true,
+    submitHandler: function(form) {
+
+      var sendData = $("form#register-form").serializeObject();
+      delete sendData.password2;
+      //for (var key in sendData) sendData[key] = "a" + sendData[key];
+
+      console.log("start submit");
+
+      /* disable orgin event behavior */
+      $.ajax({
+        type: "POST" ,
+        dataType: "json",
+        data: sendData,
+        url: '/register',
+        success: function(data) {
+
+          if (!data.accountDup) {
+
+            console.log(data.accountDup);
+            document.location = data.redirectUrl;
+  
+          } else {
+
+            $("input[name=account]")
+              .addClass("error-input")
+                .next("label.myerror")
+                .text("*帳號以被註冊過")
+                .removeClass("label-hide");
+
+          }            
+        },
+        error: function(error) {
+          console.log("fail");
+        }
+      });
+
+      return false;
+      
+    },
+    rules: {
+      account: {
+        required: true
+      },
+      nickname: {
+        required: true
+      },
+      password: {
+        required: true
+      },
+      password2: {
+        required: true,
+        equalTo: "#password"
+      },
+      cellphone: {
+        required:true
+/*
+        required: true,
+        number: true
+*/
+      },
+      email: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      account: {
+        required: "*請輸入帳號"
+      },
+      nickname: {
+        required: "*請輸入稱呼"
+      },
+      password: {
+        required: "*請輸入密碼"
+      },
+      password2: {
+        required: "*請輸入確認密碼",
+        equalTo: "*輸入確認密碼錯誤"
+      },
+      cellphone: {
+        required: "*請輸入手機"
+/*      
+        required: "*請輸入手機",
+        number: "*請輸入數字格式"
+*/        
+      },
+      email: {
+        required: "*請輸入信箱",
+        email: "*信箱格式錯誤"
+      }
+    },
+    showErrors: function(errorMap, errorList) {
+      console.log(errorMap);
+      console.log($(errorList).each(function(){ console.log(this);}));
+      $(errorList).each(function(){
+        $(this.element).next(".myerror").text(this.message);
+      });
+      
+      this.defaultShowErrors();
+    },
+    errorPlacement: function(error, element) {
+      return true;
+    },
+    highlight: function(element, errorClass) {
+      $(element).addClass("error-input");
+      $(element).next(".myerror").removeClass("label-hide");
+    },
+    unhighlight: function(element, errorClass) {
+      $(element).removeClass("error-input");
+      $(element).next(".myerror").addClass("label-hide");
+    }
+  });
+
+});
