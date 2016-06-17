f4fe34603e4142eb8e1f3b747ae3e5d6eb8b20e8
Chen Hung, Lin <kk357258@gmail.com>
2016-06-17 22:09:06 +0800

    finger swipe right change plugin
    1. use hammer.js
    2. try to do swipe animated, but code commented now
       (look like terrible)

--- public/js/swipeback.js
+++ public/js/swipeback.js
@@ -1,8 +1,66 @@
 $(function(){
-  $("body").swipe({
-    swipeRight: function(event) {
-      document.location = $("head > link[rel=prev]").attr('href');
-    },
-    threshold: 75
-  });
+  var el = $("body"),
+    mc = new Hammer(el[0]);
+
+  mc.on('swipe', onSwipe);
+
+  function onSwipe (event) {
+    var angle = Math.abs(event.angle),
+      dx = event.deltaX;
+
+    if (angle < 10 && dx > 30) {
+      document.location = $('head > link[rel=prev]').attr('href');
+    }
+  }
+/*
+  var el = $("body");
+  var mc = new Hammer(el[0]);
+  mc.on("swipe", onSwipe);
+  mc.on("panstart panmove", onPanMove);
+  mc.on("panend", onPanEnd);
+
+  function onSwipe(ev) {
+    console.log();
+    var angle = Math.abs(ev.angle),
+      dx = ev.deltaX;
+    if (angle < 10 && dx > 20) {
+      // go animate
+      // direct to prev page
+        document.location = $('head > link[rel=prev]').attr('href');
+    };
+  }
+
+  function onPanMove(ev) {
+    var angle = Math.abs(ev.angle),
+      direct = ev.direction,
+      deltaX = ev.deltaX;
+
+      console.log(deltaX);
+    if (angle < 10 && (direct == Hammer.DIRECTION_RIGHT)) {
+      el.css({
+        'margin-left': deltaX
+      });
+    }
+  }
+
+  function onPanEnd(ev) {
+    var angle = Math.abs(ev.angle),
+      direct = ev.direction,
+      deltaX = ev.deltaX;
+
+      console.log(deltaX);
+    if (angle < 10 && (direct == Hammer.DIRECTION_RIGHT) && deltaX > 70) {
+      el.animate({
+        'margin-left': el.width()
+      }, (el.width()-deltaX)*2.5, function() {
+        document.location = $('head > link[rel=prev]').attr('href');
+      });
+    } else {
+      el.animate({
+        'margin-left': 0
+      }, deltaX*2.5);
+    }
+  }
+*/
+  //console.log(el);
 });
