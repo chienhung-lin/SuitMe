7f142486d3b2b5f2e9e4c98e62d85454f7b8f0bf
Chen Hung, Lin <kk357258@gmail.com>
2016-06-19 11:43:39 +0800

    suit category problem fixed

--- public/js/suitcategory.js
+++ public/js/suitcategory.js
@@ -1,25 +1,32 @@
-/*Categoryofsuit*/
-$(function() {
-  $.scrollify({
-    section : ".category",
-    sectionName : "section-name",
-    interstitialSection : "",
-    easing: "easeOutExpo",
-    scrollSpeed: 1000,
-    offset : 0,
-    scrollbars: true,
-    standardScrollElements: "",
-    setHeights: true,
-    before:function() {},
-    after:function() {},
-    afterResize:function() {},
-    afterRender:function() {}
-  });
+$(function(){
+  $(".cat_opening").click(function(){
+    $(".cat_opening").addClass("byebye");
+    $(".category").removeClass("byebye");
+    $(".cat_opening").addClass("byebye");
+    $.scrollify({
+      section : ".category",
+      // interstitialSection : ".category",
+      easing: "easeOutExpo",
+      scrollSpeed: 700,
+      offset : 0,
+      scrollbars: false,
+      setHeights: false
+    });
+  });   
 });
-					
-$(document).ready(function() {
-  $(".cat_show").click(function() {
-    $(".cat_hide").fadeIn();
-    $(".cat_show").fadeOut();
+
+$(function () {
+  var count = 0;
+  var start = new Date().getTime();
+  $('.category').click(function () {
+    count += 1;
+    var end = new Date().getTime();
+    var time = (end - start)/1000;
+    if(count %10==1) {
+      start = end;
+    }
+    if (count % 10==0) {    
+      alert("你按了10下螢幕"+'在' + time+'秒內');
+    }
   });
 });
