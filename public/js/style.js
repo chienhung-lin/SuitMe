4af4749aba57e60b4773f76cf40d66b1efed1197
Chen Hung, Lin <kk357258@gmail.com>
2016-06-19 14:36:53 +0800

    suitinfo apge revised and style.js remove useless

--- public/js/style.js
+++ public/js/style.js
@@ -10,30 +10,3 @@ $(document).ready(function(){
 		}
 	);
 });
-
-
-
-/**************** 08+09   ********************/
-
-;(function($) {        
-  $(function() {           
-    $('.s-shoulder ').bind('click', function(e) {
-      e.preventDefault();
-      $('.suit-popup.shoulder').bPopup({
-        fadeSpeed: 'slow', 
-        followSpeed: 1500, 
-      });
-    });
-  });
-})(jQuery);	
-;(function($){$(function(){$('.s-button').bind('click',function(e){e.preventDefault();$('.suit-popup.button').bPopup({fadeSpeed:'slow',followSpeed: 1500,});});});})(jQuery);	
-;(function($){$(function(){$('.s-pocket').bind('click',function(e){e.preventDefault();$('.suit-popup.pocket').bPopup({fadeSpeed:'slow',followSpeed: 1500,});});});})(jQuery);	
-;(function($){$(function(){$('.s-hand').bind('click',function(e){e.preventDefault();$('.suit-popup.hand').bPopup({fadeSpeed:'slow',followSpeed: 1500,});});});})(jQuery);	
-;(function($){$(function(){$('.s-pants').bind('click',function(e){e.preventDefault();$('.suit-popup.pants').bPopup({fadeSpeed:'slow',followSpeed: 1500,});});});})(jQuery);	
-/*appin*/
-function blinker() {
-    $('.appin').fadeOut(500);
-    $('.appin').fadeIn(500);
-}
-
-setInterval(blinker, 100);
---
+++ public/js/suitinfo.js
@@ -0,0 +1,17 @@
+/**************** 08+09   ********************/
+;(function($) {        
+        $(function() {           
+            $('.s-shoulder ').bind('click', function(e) {
+                e.preventDefault();             
+                $('.suit-popup.shoulder').bPopup({
+            fadeSpeed: 'slow', 
+            followSpeed: 1500, 
+        });
+            });
+        });
+    })(jQuery);	
+;(function($){$(function(){$('.s-tie').bind('click',function(e){e.preventDefault();$('.suit-popup.tie').bPopup({fadeSpeed:'slow',followSpeed: 1500,});});});})(jQuery);	
+;(function($){$(function(){$('.s-button').bind('click',function(e){e.preventDefault();$('.suit-popup.button').bPopup({fadeSpeed:'slow',followSpeed: 1500,});});});})(jQuery);	
+;(function($){$(function(){$('.s-pocket').bind('click',function(e){e.preventDefault();$('.suit-popup.pocket').bPopup({fadeSpeed:'slow',followSpeed: 1500,});});});})(jQuery);	
+;(function($){$(function(){$('.s-hand').bind('click',function(e){e.preventDefault();$('.suit-popup.hand').bPopup({fadeSpeed:'slow',followSpeed: 1500,});});});})(jQuery);	
+;(function($){$(function(){$('.s-pants').bind('click',function(e){e.preventDefault();$('.suit-popup.pants').bPopup({fadeSpeed:'slow',followSpeed: 1500,});});});})(jQuery);	
